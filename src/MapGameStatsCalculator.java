c3a4de759f02e9edb38ccd23db81d18400c08304
JustinS720 <274596103+JustinS720@users.noreply.github.com>
2026-04-29 12:08:49 -0700

    Configured MapGameStatsCalculator Constructor & implemented gameCount method

--- src/MapGameStatsCalculator.java
+++ src/MapGameStatsCalculator.java
@@ -30,8 +30,13 @@ public class MapGameStatsCalculator implements GameStatsCalculator {
     while(scoreInput.hasNext()) {
       String name = scoreInput.next();
       int score = scoreInput.nextInt();
-
-      // TODO: add logic here to use the name and score to fill your map(s)!
+      
+      if(gameCounts.containsKey(name)) {
+      gameCounts.put(name, gameCounts.get(name) + 1);
+      }
+      else {
+      gameCounts.put(name, 1);
+      }
     }
   }
 
@@ -45,8 +50,10 @@ public class MapGameStatsCalculator implements GameStatsCalculator {
   @Override
   public int gameCount(String person) {
     // TODO: remove this exception once you have implemented your method!
-    throw new UnsupportedOperationException("Unimplemented method 'gameCount'");
+    //throw new UnsupportedOperationException("Unimplemented method 'gameCount'");
+    checkPerson(person);
 
+    return gameCounts.get(person);
     // Uncomment this and have it as your first line once you remove the UnsupportedOperationException
     //checkPerson(person);
   }
